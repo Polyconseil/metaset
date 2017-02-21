1d3c1b1f0b0553e7a8cbb8d74eafcc613ca2a8dd
ytan <yunshi.tan@polyconseil.fr>
2017-02-21 11:22:12 +0100

    django_field: add default form field
    which could be useful for a model form and django admin

--- metaset/django_field.py
+++ metaset/django_field.py
@@ -26,11 +26,20 @@ from . import MetaSet
 
 
 def _recur_serialize_metaset(value):
-    """Transform a MetaSet to a JSON serializable value"""
+    """Transform a MetaSet to a JSON serializable value
+    """
     try:
         return {k: _recur_serialize_metaset(v) for k, v in value.items()}
     except AttributeError:
         return list(value)
+
+
+class MetaFormField(JSONFormField):
+    def prepare_value(self, value):
+        if value is None:
+            return None
+        return super(MetaFormField, self).prepare_value(
+            _recur_serialize_metaset(value))
 
 
 class MetaSetField(JSONField):
@@ -71,3 +80,8 @@ class MetaSetField(JSONField):
         if value is not None:
             value = _recur_serialize_metaset(value)
         return super(MetaSetField, self).validate(value, model_instance)
+
+    def formfield(self, **kwargs):
+        defaults = {'form_class': MetaFormField}
+        defaults.update(kwargs)
+        return super(MetaSetField, self).formfield(**defaults)
